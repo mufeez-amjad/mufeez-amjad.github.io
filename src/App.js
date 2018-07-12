9fa067dab2b17a4c1e35f2feb93614ac3c9a9487
Mufeez Amjad <zeefumdajma@outlook.com>
2018-07-11 22:46:39 -0400

    WIP
    Added flexbox for content, some mobile styling.
    
    Finished intro portion + styling

--- src/App.js
+++ src/App.js
@@ -1,19 +1,51 @@
 import React, { Component } from 'react';
 import './App.css';
 import logo from './logo.png';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGithub } from '@fortawesome/free-brands-svg-icons'
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons'
+import { faFilePdf } from '@fortawesome/free-solid-svg-icons'
 
 class App extends Component {
+
+  constructor(){
+    super();
+    this.state = {
+      opacity: 0,
+    };
+    this.scrollEvent = this.scrollEvent.bind(this);
+}
+
+  scrollEvent() {
+    this.setState({
+      opacity: this.state.opacity + 1,
+    });
+  }
+
   render() {
     return (
-      <div className="app">
+      <div className="app" onScroll={this.scrollEvent}>
+        
         <div className="intro">
           <img src={logo} alt="logo" />
           <h1>Mufeez Amjad</h1>
           <h2>Developer and Designer</h2>
-          {/* add social media links here */}
+          <div className="social-media">
+            <a href='https://github.com/mufeez-amjad' target='blank'><FontAwesomeIcon id="github" icon={faGithub} size="2x"/></a>
+            <a href='https://linkedin.com/in/mufeezamjad' target='blank'><FontAwesomeIcon icon={faLinkedin} size="2x"/></a>
+            <a href='' target='blank'><FontAwesomeIcon icon={faFilePdf} size="2x"/></a>
+          </div>
         </div>
-        <div className="scrollable">
-          {/* TODO: add content here for scrolling down, two columns using flexbox */}
+
+        <div className="scrollable" style={{opacity: this.state.opacity}}>
+          <h1>Hello world!</h1>
+          
+          <div className="profile">
+            <img src={logo} alt="Profile"/>
+            <p>I’m an incoming <em>Software Engineering</em> student at the University of Waterloo
+                with experience in both <em>software development</em> and <em>graphic design</em>.</p>
+          </div>
+          
         </div>
       </div>
     );
--- src/index.js
+++ src/index.js
@@ -3,13 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import WebFont from 'webfontloader';
+//import WebFont from 'webfontloader';
 
-WebFont.load({
-  google: {
-    families: ['Montserrat: 600', 'Raleway: 500, 500i, 600, 700, 800']
-  }
-});
+// WebFont.load({
+//   google: {
+//     families: ['Montserrat: 600', 'Raleway: 500, 500i, 600, 700, 800']
+//   }
+// });
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
