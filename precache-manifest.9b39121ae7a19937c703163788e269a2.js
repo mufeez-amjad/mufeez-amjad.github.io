ddddbf25b7859e965e457a1af144b6ef540d3e4d
mufeez-amjad <zeefumdajma@outlook.com>
2019-03-08 11:17:28 -0500

    updated resume

---
+++ precache-manifest.9b39121ae7a19937c703163788e269a2.js
@@ -0,0 +1,138 @@
+self.__precacheManifest = [
+  {
+    "revision": "89cabeaea5e86c7c97b433a0a7435048",
+    "url": "/static/media/nike.89cabeae.png"
+  },
+  {
+    "revision": "29e5621b271f2a829975",
+    "url": "/static/css/main.0cd2f942.chunk.css"
+  },
+  {
+    "revision": "04985f3dd383656328e8",
+    "url": "/static/js/1.04985f3d.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "a4fbb8161d710a19e5182f410585bcca",
+    "url": "/static/media/MufeezAmjadResume.a4fbb816.pdf"
+  },
+  {
+    "revision": "18d3756ea37ad800a5fd3345db7f2a63",
+    "url": "/static/media/bg.18d3756e.png"
+  },
+  {
+    "revision": "a2cc482b5021be35a27c27562c4a713d",
+    "url": "/static/media/socket.a2cc482b.png"
+  },
+  {
+    "revision": "c5fdefd3c5962958f30850aee21a5a5c",
+    "url": "/static/media/unity.c5fdefd3.png"
+  },
+  {
+    "revision": "235c85720b15325e9cb3c053a1f46fdd",
+    "url": "/static/media/app-actuator.235c8572.png"
+  },
+  {
+    "revision": "b73ab840cb6224e267135c1e293bcf40",
+    "url": "/static/media/april2017.b73ab840.png"
+  },
+  {
+    "revision": "a04445813e86435ff5b1b9e290529075",
+    "url": "/static/media/budgeting.a0444581.png"
+  },
+  {
+    "revision": "958dcde7c5fadf602a7269c4de5aa75a",
+    "url": "/static/media/crywolf.958dcde7.png"
+  },
+  {
+    "revision": "b45c5ddbf6c3b2a376f62ce4894eac00",
+    "url": "/static/media/feedme.b45c5ddb.png"
+  },
+  {
+    "revision": "9a984066b50e0b8af65572f25e159323",
+    "url": "/static/media/flipd.9a984066.png"
+  },
+  {
+    "revision": "d3d3f5aeb8b7bb244bf34add84e173dc",
+    "url": "/static/media/fractal.d3d3f5ae.png"
+  },
+  {
+    "revision": "c9d40ade838eee636008d2d275330393",
+    "url": "/static/media/hoc.c9d40ade.png"
+  },
+  {
+    "revision": "7fdef97efbd10162ffab9bd8df5e7c1a",
+    "url": "/static/media/intro.7fdef97e.png"
+  },
+  {
+    "revision": "29e5621b271f2a829975",
+    "url": "/static/js/main.29e5621b.chunk.js"
+  },
+  {
+    "revision": "86cc71a632fa93a7ccfd59efe470d27e",
+    "url": "/static/media/park.86cc71a6.png"
+  },
+  {
+    "revision": "869ba1d64580f5ed7dd478b91988f1af",
+    "url": "/static/media/phs.869ba1d6.png"
+  },
+  {
+    "revision": "30c8b7b7c49b649f12aa12007fbd4731",
+    "url": "/static/media/phs2.30c8b7b7.png"
+  },
+  {
+    "revision": "a8e66e724791225ebb077355d2f73579",
+    "url": "/static/media/pickering.a8e66e72.png"
+  },
+  {
+    "revision": "83866e1d20a97928650820a68a460ca8",
+    "url": "/static/media/pickering2.83866e1d.png"
+  },
+  {
+    "revision": "c6a53c257aa8c1da87dec7332c4f3e77",
+    "url": "/static/media/science-phd.c6a53c25.png"
+  },
+  {
+    "revision": "d61a2af15679a169268c0664d8841120",
+    "url": "/static/media/switch-connect.d61a2af1.png"
+  },
+  {
+    "revision": "5285bf5322dad03b416fa2e636f7ad88",
+    "url": "/static/media/tesla.5285bf53.jpg"
+  },
+  {
+    "revision": "be39b77ec8821544f79b27cd0673fd61",
+    "url": "/static/media/tesla2.be39b77e.png"
+  },
+  {
+    "revision": "0a60dac9e61edd430b9203d25d0ab448",
+    "url": "/static/media/tesla3.0a60dac9.png"
+  },
+  {
+    "revision": "ba33d111e48256b7604371f6dceaa5f5",
+    "url": "/static/media/trialsloop.ba33d111.png"
+  },
+  {
+    "revision": "b5207248c18d974a0d06b9731a9bc7e2",
+    "url": "/static/media/trojans.b5207248.png"
+  },
+  {
+    "revision": "b365d20522320ffbc790b9e6a711fb6e",
+    "url": "/static/media/waterloo.b365d205.png"
+  },
+  {
+    "revision": "8481a943cc214a380d7f96c470d1f75c",
+    "url": "/static/media/waterloo2.8481a943.png"
+  },
+  {
+    "revision": "8bffc635d1762f583dee5558fba8c87e",
+    "url": "/static/media/wisseo.8bffc635.png"
+  },
+  {
+    "revision": "aee958210252acb587b01d47c855f7d8",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.26e0480f983288340b179acfb4453eff.js"
+  "/precache-manifest.9b39121ae7a19937c703163788e269a2.js"
 );
 
 workbox.clientsClaim();
