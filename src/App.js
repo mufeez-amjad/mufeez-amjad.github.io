146129cb2b2518ddd88139efacbcb50213cf1a6e
Mufeez Amjad <mufeez.amjad@outlook.com>
2019-06-09 01:03:22 -0400

    fixed awards description

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
-import React, { Component } from 'react';
-import './App.css';
-import profile from './profile.jpg';
-import Intro from './Components/Intro/Intro';
-import Experience from './Components/Experience/Experience';
-import Portfolio from './Components/Portfolio/Portfolio';
-import Awards from './Components/Awards/Awards'
+import React, { Component } from "react";
+import "./App.css";
+import profile from "./profile.jpg";
+import Intro from "./Components/Intro/Intro";
+import Experience from "./Components/Experience/Experience";
+import Portfolio from "./Components/Portfolio/Portfolio";
+import Awards from "./Components/Awards/Awards";
 
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faChevronDown } from '@fortawesome/free-solid-svg-icons'
-import ReactGA from 'react-ga';
-import {Helmet} from "react-helmet";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faChevronDown } from "@fortawesome/free-solid-svg-icons";
+import ReactGA from "react-ga";
+import { Helmet } from "react-helmet";
 
 //TODO: add project modals
-//TODO: reduce file size for images
-//TODO: fix image size
-
 //TODO: https://css-tricks.com/animate-a-container-on-mouse-over-using-perspective-and-transform/
+
+//TODO: add dividers between jobs/awards
+//TODO: change dev portfolio to not be square on mobile (maybe rectangle)
 
 class App extends Component {
   constructor() {
@@ -28,8 +28,8 @@ class App extends Component {
   }
 
   initializeReactGA() {
-    ReactGA.initialize('UA-131240409-1');
-    ReactGA.pageview('/home');
+    ReactGA.initialize("UA-131240409-1");
+    ReactGA.pageview("/home");
   }
 
   componentWillMount() {
@@ -39,11 +39,16 @@ class App extends Component {
 
   componentDidMount() {
     if (window.screen.width >= 600) {
-      this.scrollTo.current.scrollIntoView({behavior: "smooth", block: "start", inline: "nearest"});
+      this.scrollTo.current.scrollIntoView({
+        behavior: "smooth",
+        block: "start",
+        inline: "nearest"
+      });
     }
   }
 
-  addMonths = (oldDate, nMonths) => new Date(oldDate.setMonth(oldDate.getMonth() + nMonths)); 
+  addMonths = (oldDate, nMonths) =>
+    new Date(oldDate.setMonth(oldDate.getMonth() + nMonths));
 
   calculateTerm() {
     const today = new Date(); //"01/07/2019"
@@ -52,62 +57,54 @@ class App extends Component {
     //Fall: Sept 6
     //Winter: Jan 7
     //Spring: May 6
-    var oneA = new Date('September 2, 2018');
-    var oneB = new Date('January 7, 2019');      
-    var twoA = new Date('September 5, 2019'); //Fall -> Spring
-    var twoB = new Date('May 4, 2020'); //Spring -> Winter
-    var threeA = new Date('January 4, 2021'); //Winter -> Fall
-    var threeB = new Date('September 9, 2021'); //Fall -> Spring    
-    var fourA = new Date('May 9, 2022'); //Spring -> Winter
-    var fourB = new Date('January 9, 2023');
-    var graduation = new Date('June 1, 2018');
+    var oneA = new Date("September 2, 2018");
+    var oneB = new Date("January 7, 2019");
+    var twoA = new Date("September 5, 2019"); //Fall -> Spring
+    var twoB = new Date("May 4, 2020"); //Spring -> Winter
+    var threeA = new Date("January 4, 2021"); //Winter -> Fall
+    var threeB = new Date("September 9, 2021"); //Fall -> Spring
+    var fourA = new Date("May 9, 2022"); //Spring -> Winter
+    var fourB = new Date("January 9, 2023");
+    var graduation = new Date("June 1, 2018");
 
-    if (today < oneA){
-      currentTerm = 'an incoming';
-    }
-    else if (today < oneB) {
+    if (today < oneA) {
+      currentTerm = "an incoming";
+    } else if (today < oneB) {
       currentTerm = "a 1A";
-    }
-    else if (today < twoA) {
+    } else if (today < twoA) {
       currentTerm = "a 1B";
       coopTerm = "Summer 2019";
       if (today > this.addMonths(oneB, 4)) {
         coopTerm = "Winter 2020";
       }
-    }
-    else if (today < twoB) {
+    } else if (today < twoB) {
       currentTerm = "a 2A";
       coopTerm = "Winter 2020";
       if (today > this.addMonths(twoA, 4)) {
         coopTerm = "Fall 2020";
       }
-    }
-    else if (today < threeA) {
+    } else if (today < threeA) {
       currentTerm = "a 2B";
       coopTerm = "Fall 2020";
       if (today > this.addMonths(twoB, 4)) {
         coopTerm = "Summer 2021";
       }
-    }
-    else if (today < threeB) {
+    } else if (today < threeB) {
       currentTerm = "a 3A";
       coopTerm = "Summer 2021";
       if (today > this.addMonths(threeA, 4)) {
         coopTerm = "Winter 2022";
       }
-    }
-    else if (today < fourA) {
+    } else if (today < fourA) {
       currentTerm = "a 3B";
       coopTerm = "Winter 2022";
       if (today > this.addMonths(threeB, 4)) {
         coopTerm = "Fall 2022";
       }
-    }
-    else if (today < fourB) {
+    } else if (today < fourB) {
       currentTerm = "a 4A";
       coopTerm = "Fall 2022";
-    }
-    else if (today < graduation) {
+    } else if (today < graduation) {
       currentTerm = "a 4B";
     }
 
@@ -118,45 +115,72 @@ class App extends Component {
   }
 
   render() {
-
     return (
       <div className="app">
-            <Helmet>
-                <meta charSet="utf-8" />
-                <meta name="author" content="Mufeez Amjad"/>
-                <meta name="description" content="Mufeez Amjad — Software Engineer — Toronto, CA"/>
-                <meta name="viewport" content="width=device-width, initial-scale=1.0"/>     
-                <meta name="keywords" content="Mufeez Amjad, Waterloo, Software Engineer, Designer, Toronto" />
-            </Helmet>
+        <Helmet>
+          <meta charSet="utf-8" />
+          <meta name="author" content="Mufeez Amjad" />
+          <meta
+            name="description"
+            content="Mufeez Amjad — Software Engineer — Toronto, CA"
+          />
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1.0"
+          />
+          <meta
+            name="keywords"
+            content="Mufeez Amjad, Waterloo, Software Engineer, Designer, Toronto"
+          />
+        </Helmet>
         <Intro />
 
         <div className="scrollable">
-        
-          <a className="chevron"> 
-            <FontAwesomeIcon color="#35a6de" icon={faChevronDown} size="2x"/>
+          <a className="chevron">
+            <FontAwesomeIcon color="#35a6de" icon={faChevronDown} size="2x" />
           </a>
-         
-          <h1 ref={this.scrollTo}>
-            Hey there!
-          </h1>
+
+          <h1 ref={this.scrollTo}>Hey there!</h1>
 
           <div className="profile">
             <img src={profile} alt="Profile" />
-            <p className="summary">I’m {this.state.term} <em>Software Engineering</em> student at the University of Waterloo
-                with experience in both <em>software development</em> and <em>graphic design</em>.
+            <p className="summary">
+              I’m {this.state.term} <em>Software Engineering</em> student at the
+              University of Waterloo with experience in both{" "}
+              <em>software development</em> and <em>graphic design</em>.
             </p>
           </div>
-          
-          <div style={{padding: '10px 0 0 0'}}>
-            <p style={{textAlign: 'center'}}><b>Currently looking for <span style={{color: '#35a6de'}}>{this.state.coop}</span> software internships!</b></p>
+
+          <div
+            style={{
+              padding: "10px 0 0 0",
+              marginLeft: "15px",
+              marginRight: "15px"
+            }}>
+            <p style={{ textAlign: "center" }}>
+              <b>
+                Currently looking for{" "}
+                <span style={{ color: "#35a6de" }}>{this.state.coop}</span>{" "}
+                software internships!
+              </b>
+            </p>
           </div>
 
           <div className="skills">
             <p className="left">I have worked with:</p>
-            <p className='list'>
-              <span className="languages"><em>Python</em> <em>Java</em> <em>C/C++</em> <em>Swift</em> <em>JavaScript</em> <em>C#</em> </span>
-              <span className="frameworks"><em>React.js</em> <em>Node.js</em> <em>Socket.IO</em> <em>Flask</em> <em>React Native</em> <em>MongoDB</em> <em>Firebase</em> <em>Git</em></span> 
-              <span className="software"><em>Adobe Creative Suite</em> <em>Sketch</em></span> 
+            <p className="list">
+              <span className="languages">
+                <em>Python</em> <em>Java</em> <em>C/C++</em> <em>Swift</em>{" "}
+                <em>JavaScript</em> <em>C#</em>{" "}
+              </span>
+              <span className="frameworks">
+                <em>React.js</em> <em>Node.js</em> <em>Socket.IO</em>{" "}
+                <em>Flask</em> <em>React Native</em> <em>MongoDB</em>{" "}
+                <em>Firebase</em> <em>Git</em>
+              </span>
+              <span className="software">
+                <em>Adobe Creative Suite</em> <em>Sketch</em>
+              </span>
             </p>
           </div>
 
@@ -165,9 +189,7 @@ class App extends Component {
           <Awards />
 
           <Portfolio />
-
         </div>
-
       </div>
     );
   }
--- src/Components/Awards/Awards.js
+++ src/Components/Awards/Awards.js
@@ -1,43 +1,73 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import './Awards.css'
+import "./Awards.css";
 
 class Experience extends Component {
- render() {
+  render() {
     return (
-        <div className="awards">
-            <h2>Awards & Achievements</h2>     
+      <div className="awards">
+        <h2>Awards & Achievements</h2>
 
-            <div className="award">
-                <h3><a target='blank' href="" className="strike">Waterloo Engineering Competition</a></h3>
-                <h5>Nov 2018</h5>
-                <p>Awarded for my contributions to the Microsoft Documentation for Azure during the Canadian Open Source Doc-a-thon.</p>
-            </div>    
-
-            <div className="award">
-                <h3><a target='blank' href="https://docs.microsoft.com/en-us/azure/" className="strike">Microsoft Top Contributor for Azure Docs</a></h3>
-                <h5>Nov 2018</h5>
-                <p>Awarded for my contributions to the Microsoft Documentation for Azure during the Canadian Open Source Doc-a-thon.</p>
-            </div>
-
-            <div className="award">
-                <h3><a target='blank' href="https://www.pickering.ca/en/city-hall/civicawards.aspx" className="strike">City of Pickering Civic Award</a></h3>
-                <h5>June 2018</h5>
-                <p>Awarded to a Pickering resident who has exemplified outstanding service and achievements.</p>
-            </div>
-
-            <div className="award">
-                <h3><a target='blank' href="http://ecoocs.org/" className="strike">2nd Place - ECOO Programming Competition</a></h3>
-                <h5>Mar 2018</h5>
-                <p>Placed second in Round 1 and top 20 out of 200+ teams at the Central Ontario Finals (April 2018).</p>
-            </div>
-            
-            
-
+        <div className="award">
+          <h3>
+            <a target="blank" href="" className="strike">
+              Waterloo Engineering Competition
+            </a>
+          </h3>
+          <h5>Nov 2018</h5>
+          <p>
+            Placed third at the annual competition for building a genetic AI for{" "}
+            <i>Tron</i>.
+          </p>
         </div>
+
+        <div className="award">
+          <h3>
+            <a
+              target="blank"
+              href="https://docs.microsoft.com/en-us/azure/"
+              className="strike">
+              Microsoft Top Contributor for Azure Docs
+            </a>
+          </h3>
+          <h5>Nov 2018</h5>
+          <p>
+            Awarded for my contributions to the Microsoft Documentation for
+            Azure during the Canadian Open Source Doc-a-thon.
+          </p>
+        </div>
+
+        <div className="award">
+          <h3>
+            <a
+              target="blank"
+              href="https://www.pickering.ca/en/city-hall/civicawards.aspx"
+              className="strike">
+              City of Pickering Civic Award
+            </a>
+          </h3>
+          <h5>June 2018</h5>
+          <p>
+            Awarded to a Pickering resident who has exemplified outstanding
+            service and achievements.
+          </p>
+        </div>
+
+        <div className="award">
+          <h3>
+            <a target="blank" href="http://ecoocs.org/" className="strike">
+              2nd Place - ECOO Programming Competition
+            </a>
+          </h3>
+          <h5>Mar 2018</h5>
+          <p>
+            Placed second in Round 1 and top 20 out of 200+ teams at the Central
+            Ontario Finals (April 2018).
+          </p>
+        </div>
+      </div>
     );
   }
 }
-
 
 export default Experience;
