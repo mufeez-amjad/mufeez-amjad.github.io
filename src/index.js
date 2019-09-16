26ff6f56a6029381c2ed9f0c6e32be7bbe3d7b98
Mufeez Amjad <mufeez.amjad@outlook.com>
2019-09-16 19:51:20 -0400

    reset cache - commented out

--- src/index.js
+++ src/index.js
@@ -8,3 +8,13 @@ import { unregister } from './registerServiceWorker';
 ReactDOM.render(< App />, document.getElementById('root'));
 {/* registerServiceWorker(); */ }
 unregister();
+
+// caches.keys().then(async (cacheNames) => {
+//     return await Promise.all(
+//         cacheNames.filter((cacheName) => {
+//             return true
+//         }).map((cacheName) => {
+//             return caches.delete(cacheName);
+//         })
+//     );
+// })
